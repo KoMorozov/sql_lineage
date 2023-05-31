95e63135c59f0d92c75bfd1c4e0a7ca8431017dc
Konstantin <morozz156@gmail.com>
2023-05-31 17:33:44 +0300

    123

--- lineage.py
+++ lineage.py
@@ -1,1 +1,26 @@
-123
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "/scripts"
+script_files_list = []
+
+for path in os.listdir(dir_path):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(dir_path, path)):
+        script_files_list.append(path)
+
+
+def get_tables(file, file_name):
+    split_queries = file.split(';')
+    for i in split_queries:
+        print(i, file_name)
+
+
+
+for script in script_files_list:
+    with open(os.path.dirname(os.path.realpath(__file__)) + "/scripts" + f"/{script}") as f:
+        contents = f.read()
+        get_tables(contents, script)
+
+
+
+
