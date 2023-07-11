bec08312ed7432b150158a70491333a0284f1ad4
Konstantin <morozz156@gmail.com>
2023-07-11 15:58:52 +0300

    123

--- lineage.py
+++ lineage.py
@@ -3,16 +3,18 @@ import re
 import networkx as nx
 import matplotlib.pyplot as plt
 
-dir_path = os.path.dirname(os.path.realpath(__file__)) + "/scripts"
-script_files_list = []
-list_of_insert = []
-list_of_tables = []
+dir_path = os.path.dirname(os.path.realpath(__file__)) + "\scripts"
+
 schemas = ['db', 'db2']
 
-for path in os.listdir(dir_path):
-    # check if current path is a file
-    if os.path.isfile(os.path.join(dir_path, path)):
-        script_files_list.append(path)
+script_files_list = []
+
+for path, subdirs, files in os.walk(dir_path):
+    for name in files:
+        if ".txt" in name:
+            script_files_list.append(os.path.join(path, name))
+
+list_of_insert = []
 
 
 def get_queries(file):
@@ -24,32 +26,30 @@ def get_queries(file):
 
 
 for script in script_files_list:
-    with open(os.path.dirname(os.path.realpath(__file__)) + "/scripts" + f"/{script}") as f:
+    with open(script) as f:
         contents = f.read()
         get_queries(contents)
 
+list_of_tables = []
+
 for i in list_of_insert:
-    list_of_tables.append(re.findall(r'db.\w+|db2.\w+', i))
+    list_of_tables.append(re.findall(r'db.\w+|db2.\w+|dm.\w+', i))
 
 tuples_of_connections = []
 for script in list_of_tables:
-        for i in script:
-            if i != script[0]:
-                tuples_of_connections.append(tuple([i,script[0]]))
-
+    for i in script:
+        if i != script[0]:
+            tuples_of_connections.append(tuple([i, script[0]]))
 
 first_tables = []
 
 for tables in list_of_tables:
     first_tables.append(tables[0])
 
-#first_tables = list(dict.fromkeys(first_tables))
-
 G = nx.DiGraph()  # создаём объект графа
 
 nodes = first_tables
 edges = tuples_of_connections
-
 
 # добавляем информацию в объект графа
 G.add_nodes_from(nodes)
